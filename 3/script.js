28f9b33c5d5e5c8d710620bbfdce9d8a8160c61f
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-10-19 13:46:47 +0200

    Uppgift 3

--- 3/script.js
+++ 3/script.js
@@ -1,3 +1,6 @@
-let Blah = ["hejsan", "svejsan" , "hur" , "mår" , "du"]
+let Blah = ["hejsan" , "svejsan" , "hur" , "mår" , "du"]
 
-console.log(Blah.length)
+// console.log(Blah.length)
+
+Blah = ["1" , "svejsan" , "hur" , "mår" , "2"]
+
